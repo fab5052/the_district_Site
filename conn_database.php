a64e835e0a98f0d113a5a3e133a8bf36cf3b3d04
Fabrice <f.beaujois@laposte.net>
2024-03-19 15:09:46 +0100

    cmt

--- conn_database.php
+++ conn_database.php
@@ -1,11 +1,14 @@
 <?php
 
-    try
-    {
-        $conn = new PDO('mysql:host=localhost;charset=utf8;dbname=the_district', 'admin', 'Afpa1234');
+function connect_database () {
+    try {
+        $conn = new PDO("mysql:host=localhost;dbname=the_district", "admin", "Afpa1234");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connecté à la base de données:<br>";
+        return $conn;
     } catch (Exception $e) {
         echo "Erreur : " .$e->getMessage() . "<br>";
         echo "N° :" .$e->getCode();
         die("Fin du script");
     }
+}
